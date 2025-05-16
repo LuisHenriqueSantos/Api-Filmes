7df5535aaeb4dc33d4938c60cca454a108ceab85
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2025-05-16 16:17:53 -0300

    Update FilmeController.java

--- Api-Filmes/src/main/java/com/mentorama/apifilmes/controllers/FilmeController.java
+++ Api-Filmes/src/main/java/com/mentorama/apifilmes/controllers/FilmeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/filme")
+@RequestMapping("/api/filme")
 public class FilmeController {
 
     @Autowired
